67cffdd8150b32faaedc5dd7a03d0c0b340a6265
isamrx73 <isamrx73@gmail.com>
2020-07-29 21:12:32 +0900

    second commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ namespace CSTest
 {
     class Program
     {
+        static void Hello(string s){
+            Console.WriteLine($"Hello {s}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hello("World!");
         }
     }
 }
